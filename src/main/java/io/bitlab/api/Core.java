189b3bedda7e30fa5c0086d8e8f3a6c1c0ffbd8e
jtrejosb <jtrejosb@live.com>
2023-04-02 11:35:58 -0500

    Initial commit

---
+++ src/main/java/io/bitlab/api/Core.java
@@ -0,0 +1,7 @@
+package io.bitlab.api;
+
+public class Core {
+  public static void main(String[] args) {
+    System.out.println("Ok!");
+  }
+}
---
+++ src/test/java/io/bitlab/api/CoreTest.java
@@ -0,0 +1,14 @@
+package io.bitlab.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CoreTest {
+  @Test
+  void shouldBeEquals() {
+    String expected="";
+    String actual="";
+    assertEquals(expected,actual);
+  }
+}
