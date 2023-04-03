3e5a71ecc8b10c0231766513de61b6e6d391fca1
jtrejosb <jtrejosb@live.com>
2023-04-03 11:33:39 -0500

    everything running correctly

---
+++ src/main/java/io/bitlab/api/launch/AppLauncher.java
@@ -0,0 +1,24 @@
+/**
+ * @author JohnnyTB (jtrejosb@live.com)
+ * gitlab.com/iolinker | github.com/jtrejosb
+ *
+ * Licenses GNU GPL v3.0 and Eclipse Public License 2.0
+ * Date: 02/04/2023, Time: 18:42:52
+ */
+package io.bitlab.api.launch;
+
+import io.bitlab.api.Core;
+
+public class AppLauncher {
+  public static void main(String[] args) {
+    Core core=new Core();
+    for(int i=0;i<=777;i++) {
+      String output=core.getGrantString(String.valueOf(i));
+      if(output!=null) {
+        System.out.printf("chmod value: %s%npermissions: %s%n",i,output);
+      } else {
+        System.out.printf("Invalid value '%s'%n",i);
+      }
+    }
+  }
+}
--- src/test/java/io/bitlab/api/CoreTest.java
+++ src/test/java/io/bitlab/api/CoreTest.java
@@ -5,10 +5,44 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CoreTest {
+  private Core grant=new Core();
   @Test
-  void shouldBeEquals() {
-    String expected="";
-    String actual="";
+  void shouldBeEquals441() {
+    String val="441";
+    String expected="r--r----x";
+    String actual=grant.getGrantString(val);
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void shouldBeEquals043() {
+    String val="43";
+    String expected="---r---wx";
+    String actual=grant.getGrantString(val);
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void shouldBeEquals664() {
+    String val="644";
+    String expected="rw-r--r--";
+    String actual=grant.getGrantString(val);
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void shouldBeEquals563() {
+    String val="563";
+    String expected="r-xrw--wx";
+    String actual=grant.getGrantString(val);
+    assertEquals(expected,actual);
+  }
+
+  @Test
+  void shouldBeNull() {
+    String val="8";
+    String expected=null;
+    String actual=grant.getGrantString(val);
     assertEquals(expected,actual);
   }
 }
